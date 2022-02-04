0f0db62bb0086be6181fcaa5e8135b721638ff3a
saeidpiryaee <piryaeesaeid@gmail.com>
2022-02-04 15:58:31 +0330

    install browserify

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+var _ = require('loadash');
+console.log(_);
 
 color1.addEventListener("input", function(){
   changeBackground();
