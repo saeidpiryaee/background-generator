cbf2c47b1f67bf56e58121890d0e4bcc522e1d71
saeidpiryaee <piryaeesaeid@gmail.com>
2022-02-04 22:31:45 +0330

    fix lodash

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+var _ = require('lodash');
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
-
+console.log(_);
+var array = [1,2,3,4,5,6,7,8];
+console.log("The answer is : ", _.without(array,3));
 
 color1.addEventListener("input", function(){
   changeBackground();
