741e43a5e7d4eb1a2159a03223251f596c3181e9
saeidpiryaee <piryaeesaeid@gmail.com>
2022-02-04 15:37:04 +0330

    remove the import

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { without } from "lodash";
-console.log(without);
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
